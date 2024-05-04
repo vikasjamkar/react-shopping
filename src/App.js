8ac03837c817100823f9c674d2c2322fb92bfa6f
vikas <vikasjamkar2001@gmail.com>
2024-05-04 19:00:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import IndexComponent from "./Component/IndexComponent/IndexComponent";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div>
+      <IndexComponent></IndexComponent>
+    </div>
+  );
+}
+
+export default App;
